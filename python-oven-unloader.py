b419bb1850df03fa98543c800918cecb7f44e5a6
CTNeatDavid <david@clautronic.es>
2024-08-30 09:45:03 +0200

    ventilador

--- python-oven-unloader.py
+++ python-oven-unloader.py
@@ -808,11 +808,13 @@ if __name__ == '__main__':
 			client.publish('CTForn/CPUTemperature/real',str(tempC))
 			print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' T: ' + str(tempC))
 			if tempC >= tempMax and estatVentilador == estatVentiladorOFF:
+				print('Ventilador engegat')
 				gpio.output(ventiladorPin, True)
 				horaOnVentilador = datetime.now()
 				estatVentilador = estatVentiladorON
 				client.publish('CTForn/estatVentilador',estatVentilador)
 			elif tempC < tempMin and (datetime.now() - horaOnVentilador).seconds >= tempsMinimVentilador and estatVentilador == estatVentiladorON:
+				print('Ventilador apagat')
 				gpio.output(ventiladorPin, False)
 				estatVentilador = estatVentiladorOFF
 				client.publish('CTForn/estatVentilador',estatVentilador)
